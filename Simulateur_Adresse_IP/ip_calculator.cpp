3b371c7b8a477d3871698f0784dfc1ae4918fd84
Alex <144159853+Alex542277@users.noreply.github.com>
2025-06-12 20:01:52 +0100

    Update ip_calculator.cpp

--- Simulateur_Adresse_IP/ip_calculator.cpp
+++ Simulateur_Adresse_IP/ip_calculator.cpp
@@ -1,1 +1,19 @@
+#include "ipcalculator.h"
+#include <QStringList>
+
+IPCalculator::IPCalculator(const QString& ip, int mask): subnetMask(mask) {
+    parseIp(ip);
+}
+
+void IPCalculator::parseIp(const QString& ip) {
+    QStringList octets = ip.split(".");
+    int bitIndex = 24;
+    for (const QString& octet: octets) {
+        int val = octet.toInt();
+        std::bitset<8> bits(val);
+        for (int i = 0; i < 8; ++i)
+            ipBinary[bitIndex++] = bits[i];
+        bitIndex -= 16;
+}
+}
 
